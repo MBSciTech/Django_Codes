71672fbebd5f0af866363e15efa0b58ad30b933b
MB_SciTech <23002170110010@mail.ljku.edu.in>
2025-07-08 09:42:26 +0530

    Corrected project name casing in settings and configuration files from 'formProject' to 'formproject' for consistency. Updated environment variable settings accordingly.

--- formProject/formProject/asgi.py
+++ formProject/formProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for formProject project.
+ASGI config for formproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'formProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'formproject.settings')
 
 application = get_asgi_application()
--- formProject/formProject/urls.py
+++ formProject/formProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for formProject project.
+URL configuration for formproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
