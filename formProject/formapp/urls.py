3c62194533c993f28a97d5d9fa5168f7613f1120
MB_SciTech <23002170110010@mail.ljku.edu.in>
2025-07-08 08:48:41 +0530

    Removed the entire 'formapp' application, including all associated files such as models, views, forms, templates, and admin configurations, to streamline the project structure.

--- formProject/formapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/',views.register,name='register'),
-    path('register_success/',views.register_success,name='register_success')
-]
